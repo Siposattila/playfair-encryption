a6ec95835dbf795bcb56951259329043e5a44ee6
Sipos Attila <sattipolo@gmail.com>
2021-09-07 10:27:35 +0200

    c example and default user input for the key and for the text that will be encrypted also space removal function

---
+++ main.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+import numpy as np
+
+# Titkosítani kívánt szó bekérése a felhasználótól
+plaintext = input("Kérem írja be a titkosítani kívánt szót! \n")
+print("A titkosítani kívánt szó:", plaintext)
+
+# Titkosításhoz használandó kulcs bekérése a felhasználótól
+key = input("Kérem írja be a kulcsot a titkosításhoz! \n")
+print("A titkos kulcs:", key)
+
+# A könnyebb kezelés érdekében a kulcsot és a titkosítandó szót is kisbetükre konvertáljuk
+plaintext = plaintext.lower()
+key = key.lower()
+
+# Majd eltávolítjuk az összes space-t
+plaintext = RemoveSpaces(plaintext)
+key = RemoveSpaces(key)
+
+# Ez a funckció eltávolítja az összes lehetséges space-t
+def RemoveSpaces(string):
+    for i in string:
+        if i == ' ':
+            i = ''
+    return string
